29ed644f193a2820472835aa268a516d11b0ec7d
Artum Tchachkin <aptem.tchachkin@gmail.com>
2020-10-03 22:16:10 +0300

    Move configuration to .env file

--- api.py
+++ api.py
@@ -1,12 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 import time
-from flask import Flask, redirect, url_for, session, abort, request
+from flask import Flask, jsonify, abort, request
 from oauth2client import client
 
 
 app = Flask(__name__)
-app.secret_key = '7b57f060-169f-4c3f-a7a9-fa6592f12f58'
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
 app.config.from_object('config')
-
 
 
 @app.route('/', methods=['GET'])
@@ -35,7 +38,7 @@ def login():
 
     # Set path to the Web application client_secret_*.json file you downloaded from the
     # Google API Console: https://console.developers.google.com/apis/credentials
-    CLIENT_SECRET_FILE = '/home/user/Desktop/dev/thor/client_secret.json'
+    CLIENT_SECRET_FILE = os.getenv('GOOGLE_CLIENT_SECRET_FILE')
 
     # Exchange auth code for access token, refresh token, and ID token
     credentials = client.credentials_from_clientsecrets_and_code(
@@ -44,4 +47,7 @@ def login():
         auth_code)
 
     print(f"credentials = {credentials}")
-    return "OK"
+
+    resp = jsonify(success=True)
+    resp.status_code = 200
+    return resp
